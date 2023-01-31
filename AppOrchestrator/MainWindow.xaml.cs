aaf5674a9bfa12a8e1fcb045b16c00dde539f896
Crook <kamilxkruk@gmail.com>
2023-01-31 22:42:00 +0100

    Run core api programatically

--- AppOrchestrator/MainWindow.xaml.cs
+++ AppOrchestrator/MainWindow.xaml.cs
@@ -11,7 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using System.Diagnostics;
 
 namespace AppOrchestrator
 {
@@ -23,6 +27,50 @@ namespace AppOrchestrator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private Process apiProcess = null;
+
+        private void Api_ToggleClick(object sender, RoutedEventArgs e)
+        {
+            const string apiProjectName = "WebAPI";
+
+            var orchestratorDir = AppContext.BaseDirectory;
+            var pathLength = orchestratorDir.Count(c => c == '\\')-4;
+            var lastBackslashPosition = orchestratorDir.TakeWhile(c => (pathLength -= (c == '\\' ? 1 : 0)) > 0).Count();
+            var rootPath = orchestratorDir.Substring(0, lastBackslashPosition);
+            
+            var apiDir = Path.Combine(rootPath, apiProjectName);
+            
+            if(apiProcess == null )
+            {
+                apiProcess = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = "dotnet",
+                        Arguments = $"run --launch-profile https {apiProjectName}.csproj",
+                        UseShellExecute = false,
+                        WorkingDirectory = apiDir,
+                        CreateNoWindow = true
+                    }
+                };
+                apiProcess.Start();
+            }
+            else
+            {
+                apiProcess.Kill();
+                apiProcess.WaitForExit();
+                apiProcess.Dispose();
+            }
+        }
+        private void Wpf_ToggleClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void WebUI_ToggleClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
